7c30348cec039f158ddade00afe3f17856c1b905
nishant <nishant710017@gmail.com>
2022-10-29 22:54:59 +0530

    set/vector

---
+++ geekforgeeks/merge_k_sortedarray.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+#define N 105
+using namespace std;
+void printArray(vector<int> arr, int size)
+{
+for (int i=0; i < size; i++)
+	cout << arr[i] << " ";
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution
+{
+    public:
+    //Function to merge k sorted arrays.
+    vector<int> mergeKArrays(vector<vector<int>> arr, int K)
+    {
+        //code here
+        int i,j;
+        multiset<int>x;
+        for(i=0;i<K;i++)
+        {
+            // x.push_back(arr[i])
+            for(j=0;j<K;j++)
+            {
+                x.insert(arr[i][j]);
+            }
+        }
+        vector<int>b;
+        for(auto it:x)
+        {
+            b.push_back(it);
+        }
+        return b;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+	    int k;
+	    cin>>k;
+	    vector<vector<int>> arr(k, vector<int> (k, 0));
+	    for(int i=0; i<k; i++){
+	        for(int j=0; j<k; j++)
+	        {
+	            cin>>arr[i][j];
+	        }
+	    }
+	    Solution obj;
+    	vector<int> output = obj.mergeKArrays(arr, k);
+    	printArray(output, k*k);
+    	cout<<endl;
+    }
+	return 0;
+}
+
+
+
+
+
+
+// } Driver Code Ends
