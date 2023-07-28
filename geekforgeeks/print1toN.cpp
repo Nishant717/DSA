bffc725351498cdf932fe8cb31b1e6e6080619e7
Nishant717 <109461145+Nishant717@users.noreply.github.com>
2023-07-28 23:56:45 +0530

    recursion

---
+++ geekforgeeks/print1toN.cpp
@@ -0,0 +1,37 @@
+// void nums(int n , vector<int>&v){
+
+//     if(n==1){
+
+//         v.push_back(1);
+
+//         return;
+
+//     }
+
+//     else{
+
+//         nums(n-1,v);
+
+//        v. push_back(n);
+
+//     }
+
+// }
+
+ 
+
+// vector<int> printNos(int x) {
+
+//     vector<int>v;
+
+//     nums(x,v);
+
+//     return v;
+
+    // Write Your Code Here
+
+ 
+
+    
+
+// }
