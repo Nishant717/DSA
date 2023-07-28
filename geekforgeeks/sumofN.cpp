ae1dd91851cdaef28c7ffbfb63ac1ba0ff2b8c70
Nishant717 <109461145+Nishant717@users.noreply.github.com>
2023-07-29 00:02:46 +0530

    recursion

---
+++ geekforgeeks/sumofN.cpp
@@ -0,0 +1,29 @@
+// void RecursiveSum(int &sum,int n){
+
+//     if(n==0)
+
+//     return;
+
+ 
+
+//     sum = sum+n;
+
+//     RecursiveSum(sum,n-1);
+
+// }
+
+ 
+
+// int sumFirstN(int n) {
+
+//     int sum;
+
+ 
+
+//     RecursiveSum(sum,n);
+
+ 
+
+//     return sum;
+
+// }
