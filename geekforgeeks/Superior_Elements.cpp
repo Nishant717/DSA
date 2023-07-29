3ec5854bf8c73892aa9d2ef72ed0ade2909a9af3
Nishant717 <109461145+Nishant717@users.noreply.github.com>
2023-07-30 00:22:09 +0530

    for loop and sort

---
+++ geekforgeeks/Superior_Elements.cpp
@@ -0,0 +1,39 @@
+// vector<int> superiorElements(vector<int>&a) {
+    // Write your code here.
+    // vector<int>x;
+    // int n=a.size();
+    // int max=a[n-1];
+    // for(int i=n-1;i>=0;i--)
+    // {
+    //     if(a[i]>=max)
+    //     {
+    //         x.push_back(a[i]);
+    //         max=a[i];
+    //     }
+    // }
+    // sort(x.begin(),x.end());
+    // return x;
+//     int n=a.size();
+
+//     vector<int> v;
+
+//     v.push_back(a[n-1]);
+
+//     int maxi=a[n-1];
+
+//     for(int i=n-2;i>=0;i--){
+
+//         if(a[i]>maxi){
+
+//             maxi=a[i];
+
+//             v.push_back(a[i]);
+
+//         }
+
+//     }
+
+//     sort(v.begin(),v.end());
+
+//     return v;
+// }
